e93d28ccb8705d9a03550ff55173b8de87af6c05
zxcvbnm123 <1401544105@qq.com>
2018-08-09 20:35:52 +0800

    首次提交

---
+++ src/views/Login.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <div style="margin: 20px;"></div>
+        <h2>用户名</h2>
+        <el-form :label-position="labelPosition" label-width="80px" :model="formLabelAlign">
+        <el-form-item label="名称">
+            <el-input v-model="formLabelAlign.name"></el-input>
+        </el-form-item>
+        <el-form-item label="活动区域">
+            <el-input v-model="formLabelAlign.region"></el-input>
+        </el-form-item>
+        <el-form-item label="活动形式">
+            <el-input v-model="formLabelAlign.type"></el-input>
+        </el-form-item>
+        </el-form>
+    </div>
+</template>
+
+<script>
+   export default {
+       data() {
+          return {
+
+          };
+       }
+   } 
+</script>
+
+<style>
+
+</style>
+
